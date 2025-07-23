55f65e1540d53f9f39c7f01b51893910d7f000e0
Ayush Bhosale <ayushbhosale22742@gmail.com>
2025-07-23 10:57:35 +0530

    first commit

--- Grade.js
+++ Grade.js
@@ -1,12 +1,70 @@
-let a=95
-if (a>=90)
-{
-    console.log("GRADE A")
-}
-else if (a>=75)
-{
-    console.log("GRADE B")
-}
-else{
-    console.log("GRADE C")
-}
+// let a=95
+// if (a>=90)
+// {
+//     console.log("GRADE A")
+// }
+// else if (a>=75)
+// {
+//     console.log("GRADE B")
+// }
+// else{
+//     console.log("GRADE C")
+// }
+
+// let result=(age>=18) ? "Adult":"Minor";
+// console.log(result);
+
+// let day =5;
+// switch(day)
+// {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("Invalid")
+// }
+
+// for(i=0;i<=10;i++)
+// {
+//     console.log("FOR LOOP",i)
+// }
+
+// let n=0;
+// while(n<=10)
+// {
+//     console.log("WHILE LOOP",n);
+//     n++
+// }
+
+// let b=0;
+// do
+// {
+//     console.log("DO WHILE LOOP",b)
+//     b++
+// }while(b<=10);
+
+// for(i=2;i<=10;i+=2)
+// {
+//     console.log(i)
+// }
+
+let num=null ?? 10;
+console.log(num)
--- controlflow.js
+++ controlflow.js
@@ -1,12 +1,33 @@
-a=33
-b=20
-if (b>a)
-{   console.log("b is greater");
-}
-else if (a==b) 
+// a=33
+// b=20
+// if (b>a)
+// {   console.log("b is greater");
+// }
+// else if (a==b) 
+// {
+//     console.log("a is equal to b")
+// }
+// else{
+//     console.log("a is greater")
+// }
+
+// function func(name){
+//     return console.log(`Hello how are you ${name}`);
+// }
+
+// func("Ayush")
+// func("Sahil")
+// func("Parth")
+// func("Siddhesh")
+
+function addNum(num1,num2)
 {
-    console.log("a is equal to b")
+    let n1 = Number(num1);
+    let n2 = Number(num2);
+    if(isNaN(n1)||isNaN(n2)){
+        return("Enter proper number");
+    } 
+    return n1 + n2;
 }
-else{
-    console.log("a is greater")
-}
+
+console.log(addNum(10,"20abc"));
